eac393e51dc9f1cd84670d4e421c55fed59b769f
Yerai <yeerizg@gmail.com>
2019-11-21 16:46:30 +0100

    Three Activities

---
+++ Cocktail/app/src/main/java/com/example/cocktail/Favorites.java
@@ -0,0 +1,13 @@
+package com.example.cocktail;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Favorites extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favorites);
+    }
+}
